41d7250b421c876530c788ab5ab9b0e6853efefc
pavankvvd <pavankvvd@users.noreply.github.com>
2015-06-04 13:13:45 -0700

    Changed the extension from .cc to .cpp

---
+++ stack.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+#define SIZE 5
+
+class Stack
+{
+private:
+    int stack[SIZE];
+    int top;
+public:
+    Stack();
+    ~Stack();
+    void push(int);
+    void display();
+    void pop();
+};
+
+Stack::Stack()
+{
+    top = 0;
+}
+
+void
+Stack::push(int x)
+{
+    if (top >= SIZE) {
+        cout << "stack overflow" << endl;
+        return;
+    }
+    stack[top++] = x;
+}
+
+void
+Stack::display()
+{
+    if (top == 0) {
+        cout << "Stack is empty" << endl;
+        return;
+    }
+
+    int temp = 0;
+    while(temp < top) {
+        cout << stack[temp];
+        temp++;
+    }
+    cout << endl;
+}
+
+void
+Stack::pop()
+{
+    if (top == 0) {
+        cout << "stack is empty" << endl;
+        return;
+    }
+    top--;
+}
+
+int main()
+{
+    Stack s;
+    s.push(4);
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    s.display();
+    s.pop();
+    s.display();
+    s.pop();
+    s.display();
+    return 0;
+}
